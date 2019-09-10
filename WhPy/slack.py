5ab04e1b29b529a86d2d4f504b99ef8fbda64772
Michael duBois <mdubois@mcduboiswebservices.com>
2019-09-09 21:00:04 -0600

    Initial slack file

---
+++ WhPy/slack.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class Webhook:
+    """This is a class to send messages via a Discord Webhook.
+
+    Attributes:
+        discord_endpoint (string): Discord's Webhook API Endpoint
+    """
+    def __init__():
+        pass
