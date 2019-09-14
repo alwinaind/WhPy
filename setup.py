4b1e7f8d268901d248c4aa53a24f9334495baa49
Michael duBois <mdubois@mcduboiswebservices.com>
2019-09-13 20:47:24 -0600

    v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="WhPy",
-    version="0.1.4",
+    version="0.1.5",
     author="Michael duBois",
     author_email="mdubois@mcduboiswebservices.com",
     description="A Python 3 webhook module.",
